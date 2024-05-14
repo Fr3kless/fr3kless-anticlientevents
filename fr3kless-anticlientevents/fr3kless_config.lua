6c5d6a4ae5e09364b6efb2f2eb0f03329feb379d
ogfr3kless <73782648+Fr3kless@users.noreply.github.com>
2024-05-14 13:40:50 +0200

    Add files via upload

---
+++ fr3kless-anticlientevents/fr3kless_config.lua
@@ -0,0 +1,6 @@
+Config = {}
+Config.Fr3klessAntiClientEvents.ProtectedEvents = {
+    "esx:setJob", -- example
+    "playerSpawned", -- detecting native revive for some shitty menu's 
+    "yourclienteventname"
+}
---
+++ fr3kless-anticlientevents/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'ogfr3kless'
+
+description 'Detects people using client events with any lua executors for FiveM'
+
+lua54 'yes'
+
+client_script {
+    'fr3kless_client.lua',
+}
+
+shared_script {
+    'fr3kless_config.lua',
+}
