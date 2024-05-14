8bf74ad49445490383775d89eb59f2c4d6b6cb70
ogfr3kless <73782648+Fr3kless@users.noreply.github.com>
2024-05-14 16:28:39 +0200

    Update fr3kless_client.lua

--- fr3kless-anticlientevents/fr3kless_client.lua
+++ fr3kless-anticlientevents/fr3kless_client.lua
@@ -1,8 +1,8 @@
 for k, v in pairs(Config.Fr3klessAntiClientEvents.ProtectedEvents) do
     RegisterNetEvent(v)
     AddEventHandler(v, function()
-        if rscInv == "esx_ambulancejob" or rscInv == "es_extended" and v == "playerSpawned" then return end -- detecting native revive for some shitty menu's 
         local rscInv = GetInvokingResource()
+        if rscInv == "esx_ambulancejob" or rscInv == "es_extended" and v == "playerSpawned" then return end -- detecting native revive for some shitty menu's 
         if rscInv ~= nil then
             local reason = "Detected Protected Client Event by Fr3kless-AntiClientEvents - "..v.."."
             BanPlr(reason)
